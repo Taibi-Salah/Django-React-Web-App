f48aa16764c8e6ce4a1bd932c54407801a0e90c6
Taibi-Salah <Taibi-Salah>
2024-05-31 14:38:31 +0200

    first com

---
+++ FullStack app/frontend/src/components/Form.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css";
+
+function Form({ route, method }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const name = method === "login" ? "Login" : "Register";
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+        console.log(`Submitting form to ${route} with username: ${username}`);
+
+        try {
+            const res = await api.post(route, { username, password });
+            console.log("Response:", res.data);
+            if (method === "login") {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate("/");
+            } else {
+                navigate("/login");
+            }
+        } catch (error) {
+            console.error("Error during submission:", error);
+            alert(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>{name}</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            <button className="form-button" type="submit" disabled={loading}>
+                {loading ? "Loading..." : name}
+            </button>
+        </form>
+    );
+}
+
+export default Form;
